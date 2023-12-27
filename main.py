0e91f55b2e46ce41ef0030ceecccfdbf6c0f3d7c
poluianov24 <poluianov.vlad.24@gmail.com>
2023-12-27 22:59:52 +0200

    Done hw ex-1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ try:
     n2 = (int(input("Enter the second number: ")))
     if n1 > n2:
         n1, n2 = n2, n1
-    print("Numbers are multiples of 7: ")
+    print("Numbers are multiples of 7:  ")
     for i in range(n1, n2 + 1):
         if i % 7 == 0:
             print(i)
